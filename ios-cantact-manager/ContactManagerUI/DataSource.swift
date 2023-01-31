21c542186b3a3bc24e3e4ce7bbcad961bb87f670
iyeahh <yboravender@gmail.com>
2023-01-31 14:40:47 +0900

    <feat> tableView 세부설정
    - DataSource 파일 삭제
    - UITableViewDataSource를 ViewController 확장에서 직접 채택하는 방법으로 변경
    - tableViewCell 스타일 설정

--- ios-cantact-manager/ContactManagerUI/DataSource.swift
+++ ios-cantact-manager/ContactManagerUI/DataSource.swift
@@ -7,13 +7,3 @@
 
 import UIKit
 
-class DataSource: NSObject, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        ContactManageSystem().profiles.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath)
-        return cell
-    }
-}
--- ios-cantact-manager/ContactManagerUI/ViewController.swift
+++ ios-cantact-manager/ContactManagerUI/ViewController.swift
@@ -9,12 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-    var dataSource = DataSource()
-    
+    var profiles: [Profile] {
+        Array(ContactManageSystem().profiles)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.dataSource = self
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        profiles.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "이름(나이)"
+        content.secondaryText = "연락처"
+        cell.contentConfiguration = content
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
 }
 
