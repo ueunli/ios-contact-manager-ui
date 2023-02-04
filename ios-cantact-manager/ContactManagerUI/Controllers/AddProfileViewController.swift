c0d815f4cb64d4509f0bbc0c3dbfc149918470a7
ueunli <cdenen20@gmail.com>
2023-02-04 13:59:53 +0900

    <feat> 연락처 자동변환 기능 추가
    - telTextField의 입력값 자릿수에 따라 하이픈(`-`)을 삽입하는 기능 추가
    - 새 프로필 검증을 위한 정규표현식에서도 하이픈(`-`)을 허용하도록 수정

--- ios-cantact-manager/ContactManagerUI/Controllers/AddProfileViewController.swift
+++ ios-cantact-manager/ContactManagerUI/Controllers/AddProfileViewController.swift
@@ -18,6 +18,7 @@ class AddProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        telTextField.delegate = self
         setUpTextField()
     }
 
@@ -25,6 +26,10 @@ class AddProfileViewController: UIViewController {
         nameTextField.keyboardType = .asciiCapable
         ageTextField.keyboardType = .numberPad
         telTextField.keyboardType = .phonePad
+        
+        nameTextField.delegate = self
+        ageTextField.delegate = self
+        telTextField.delegate = self
     }
 
     @IBAction func canelButtonDidTap(_ sender: UIBarButtonItem) {
@@ -72,5 +77,55 @@ class AddProfileViewController: UIViewController {
         alert.addAction(confirm)
 
         return alert
+    }
+}
+
+
+
+extension AddProfileViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard textField.keyboardType == .phonePad else { return true }
+        
+        guard let newNumber = Int(string) else { return true }
+        guard let numbers = textField.text else { return true }
+        let tel = "\(numbers)\(newNumber)"
+        
+        textField.text = insertHyphenInTel(tel)
+        return false
+    }
+    
+    func insertHyphenInTel(_ tel: String) -> String {
+        let tel = tel.replacingOccurrences(of: "-", with: "")
+        switch tel.count {
+        case ...3: return PhoneNumberRegularExpressions.oneToThree.transform(tel)
+        case ...5: return PhoneNumberRegularExpressions.fourToFive.transform(tel)
+        case ...9: return PhoneNumberRegularExpressions.sixToNine.transform(tel)
+        case 10: return PhoneNumberRegularExpressions.ten.transform(tel)
+        default: return PhoneNumberRegularExpressions.elevenOrMore.transform(tel)
+        }
+    }
+    
+    enum PhoneNumberRegularExpressions: String {
+        case oneToThree = "([0-9]{3})"
+        case fourToFive = "([0-9]{2})([0-9]{2,3})"
+        case sixToNine = "([0-9]{2})([0-9]{3})([0-9]{1,4})"
+        case ten = "([0-9]{3})([0-9]{3})([0-9]{4})"
+        case elevenOrMore = "([0-9]{3})([0-9]{4})([0-9]{4,})"
+        
+        private var regexTemplate: String {
+            switch self {
+            case .oneToThree: return "$1"
+            case .fourToFive: return "$1-$2"
+            default: return "$1-$2-$3"
+            }
+        }
+        
+        func transform(_ phoneNumber: String) -> String {
+            var result = ""
+            if let regex = try? NSRegularExpression(pattern: self.rawValue) {
+                result = regex.stringByReplacingMatches(in: phoneNumber, range: NSRange(phoneNumber.startIndex..., in: phoneNumber), withTemplate: self.regexTemplate)
+            }
+            return result
+        }
     }
 }
