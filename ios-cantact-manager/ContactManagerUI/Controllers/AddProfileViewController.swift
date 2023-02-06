647b88243844ad4915bae30758bbd6f22d35b7d2
iyeahh <yboravender@gmail.com>
2023-02-06 13:16:11 +0900

    <feat> 텍스트 필드 외의 영역을 터치 하였을 때 키보드 내리도록 구현

--- ios-cantact-manager/ContactManagerUI/Controllers/AddProfileViewController.swift
+++ ios-cantact-manager/ContactManagerUI/Controllers/AddProfileViewController.swift
@@ -21,6 +21,10 @@ class AddProfileViewController: UIViewController {
         telTextField.delegate = self
         setUpTextField()
     }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+          self.view.endEditing(true)
+    }
     
     private func setUpTextField() {
         nameTextField.keyboardType = .asciiCapable
